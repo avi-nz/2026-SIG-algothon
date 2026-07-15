f352ac83f32af80990be70f42a7c8759502fda37
avi-nz <avishai@narsey.com>
2026-07-15 16:26:51 +1200

    reset

--- QuantOrDie.py
+++ QuantOrDie.py
@@ -1,31 +1,15 @@
 import numpy as np
 
-nInst = 51
-
-LOOKBACK = 20
-QUINTILE_FRAC = 0.2
-MAX_DLR_POS = 9500.0
-
-
-def getMyPosition(prcSoFar):
-    (nins, nt) = prcSoFar.shape
-
-    if nt < LOOKBACK + 2:
-        return np.zeros(nins, dtype=int)
-
-    logp = np.log(prcSoFar)
-    trail_ret = logp[:, -1] - logp[:, -1 - LOOKBACK]
-
-    q = max(1, int(nins * QUINTILE_FRAC))
-    order = np.argsort(trail_ret)
-    losers = order[:q]     # recent underperformers -> LONG (reversal)
-    winners = order[-q:]   # recent outperformers   -> SHORT (reversal)
-
-    dollarPos = np.zeros(nins)
-    dollarPos[losers] = MAX_DLR_POS
-    dollarPos[winners] = -MAX_DLR_POS
-
-    curPrices = prcSoFar[:, -1]
-    shares = (dollarPos / curPrices).astype(int)
-
-    return shares
+nInst=51
+currentPos = np.zeros(nInst)
+def getMyPosition (prcSoFar):
+    global currentPos
+    (nins,nt) = prcSoFar.shape
+    if (nt < 2):
+        return np.zeros(nins)
+    lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-2])
+    lNorm = np.sqrt(lastRet.dot(lastRet))
+    lastRet /= lNorm
+    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:,-1]])
+    currentPos = np.array([int(x) for x in currentPos+rpos])
+    return currentPos
